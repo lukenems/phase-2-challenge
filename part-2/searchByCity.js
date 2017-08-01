5e1495b33d31d2e66609da21a796a907bdd6f1a0
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-08-01 11:42:57 -0700

    Part-2 Complete

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -1,8 +1,7 @@
 const clients = require('./clients.json')
+const city = process.argv[2]
 
-const searchTerm = process.argv[2].toLowerCase()
-
-if (typeof searchTerm === 'undefined') {
+if (typeof city === 'undefined') {
   console.warn("Please enter a valid search");
   process.exit(1)
 };
@@ -10,19 +9,19 @@ if (typeof searchTerm === 'undefined') {
 let output = []
 
 let citySearch = clients.filter(function(data){
-  if(data.city.toLowerCase().includes(searchTerm)){
-    let info = new Object();
-    info.id = data.id;
-    info.rep_name = data.rep_name;
-    info.company = data.company;
-    info.city = data.city;
-    info.state = data.state;
-    return output.push(info)
-  }
+  if(data.city.toLowerCase().includes(city.toLowerCase()) && data.city.length !== city.length){
+      console.log("Finding clients in City " + '"' + city + '"' + "...\n");
+      console.warn("Error: Either no matches exist or enter full city name in quotes");
+      process.exit(1)
+    } else if (data.city.toLowerCase().includes(city.toLowerCase())){
+        let info = new Object();
+        info.id = data.id;
+        info.rep_name = data.rep_name;
+        info.company = data.company;
+        info.city = data.city;
+        info.state = data.state;
+        return output.push(info)
+    }
 })
-
+console.log("Finding clients in City " + '"' + city + '"' + "...\n");
 console.log(output)
-
-
-
-//    id, rep_name, company, city, and state that for all clients whose city matches the provided city argument.
--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -1,8 +1,7 @@
 const clients = require ('./clients.json')
+const company = process.argv[2].toLowerCase()
 
-const searchTerm = process.argv[2].toLowerCase()
-
-if (typeof searchTerm === "undefined") {
+if (typeof company === "undefined") {
   console.warn('no search term given')
   process.exit(1)
 }
@@ -10,27 +9,15 @@ if (typeof searchTerm === "undefined") {
 let matchedItems = []
 
 const findCompanybyName = clients.filter(function(record) {
-  if (record.company.toLowerCase().includes(searchTerm)) {
+  if (record.company.toLowerCase().includes(company)) {
     const info = new Object();
       info.id = record.id;
       info.company = record.company;
       info.phone = record.phone;
 
-    //   var now = JSON.parse(info)
-    // var finished = JSON.stringify(now, null, 2)
-    // console.log("this type is:", now)
-    matchedItems.push(info)
-    return matchedItems
+      return matchedItems.push(info)
   }
 })
-
-// const final = matchedItems.map(function(pairs) {
-//   console.log(pairs.id);
-// })
-
-//this is all good with this function^^
-//now i need to go through and only select and return particular pairs
-
 
 console.log('Finding Companies with name ' + "\"" + process.argv[2] + "\"" + '...' + "\n")
 console.log(matchedItems);
